739c07311dc5b2db6f22b50d57fbc6b99e4bf2b0
camryngoins <camryngoins00@gmail.com>
2022-02-27 21:50:46 -0600

    added questions to js file

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+
+// An array of all of the questions that will be asked in the code quiz
+var questions = [
+    {
+        question: 'Which is not a primitive type in javascript?',
+
+        choiceA: 'Boolean',
+        choiceB: 'String',
+        choiceC: 'Array',
+        choiceD: 'Number',
+
+        answer: 'Array'
+    },
+    {
+        question: 'Which of the elements can be used to create a numbered list?',
+
+        choiceA: '<ul>',
+        choiceB: '<li>',
+        choiceC: '<br>',
+        choiceD: '<ol>',
+
+        answer: '<ol>'
+    },
+    {
+        question: 'How would you add a comment in CSS?',
+
+        choiceA: '/* This is a comment! */',
+        choiceB: '<-- This is a comment! -->',
+        choiceC: '// This is a comment //',
+        choiceD: 'None of the above.',
+
+        answer: '/* This is a comment! */'
+    },
+    {
+        question: 'Which HTML tag is used to define a javascrip file?',
+
+        choiceA: '<style>',
+        choiceB: '<script>',
+        choiceC: '<js>',
+        choiceD: '<css>',
+
+        answer: '<script>'
+    }
+]
