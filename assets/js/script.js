64db3a27f2966d55efa458ef6f0938eba8e124ec
camryngoins <camryngoins00@gmail.com>
2022-03-01 12:50:51 -0600

    redid the html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+// set global variables
+var headerEl = document.querySelector(".header");
+var highScoresButtonEl = document.querySelector("#high-scores");
+var timerEl = document.querySelector(".timer");
+var quizContainerEl = document.querySelector(".quiz-container");
+var startQuizEl = document.querySelector(".start-quiz");
+var quizTitleEl = document.querySelector(".quiz-title");
+var quizOverviewEl = document.querySelector(".quiz-overview");
+var buttonEl = document.querySelector(".btn");
+var startQuizButtonEl = document.querySelector("#start-btn");
+
 
 // An array of all of the questions that will be asked in the code quiz
 var questions = [
@@ -42,3 +53,11 @@ var questions = [
         answer: '<script>'
     }
 ]
+
+var getHighScore = function() {
+    // Check local storage for highscore 
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+}
